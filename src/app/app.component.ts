d9daf92a620ba233c3353c12fc6819a8c9821f42
lilithhtilil <swiecaweronika6@gmail.com>
2025-11-23 13:27:43 +0100

    Add ngx charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from "@angular/core";
   styleUrl: "./app.component.css"
 })
 export class AppComponent {
-
+  energyMix = [
+    {
+      "name": "Germany",
+      "value": 8940000
+    },
+    {
+      "name": "USA",
+      "value": 5000000
+    },
+    {
+      "name": "France",
+      "value": 7200000
+    },
+    {
+      "name": "UK",
+      "value": 6200000
+    }
+  ];
 }
