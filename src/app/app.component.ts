339d740dacbb75f8cfc0e42460167f3874b7ac75
lilithhtilil <swiecaweronika6@gmail.com>
2025-11-23 17:08:50 +0100

    Today data is fetched

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,73 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css"
 })
-export class AppComponent {
-  energyMix = [
+export class AppComponent implements OnInit {
+  energyMix: EnergyMix | null = null;
+  loadingData = [
     {
-      "name": "Germany",
-      "value": 8940000
-    },
-    {
-      "name": "USA",
-      "value": 5000000
-    },
-    {
-      "name": "France",
-      "value": 7200000
-    },
-    {
-      "name": "UK",
-      "value": 6200000
+      name: "Loading...",
+      value: 100,
     }
-  ];
+  ]
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.httpClient.get<EnergyMixDto>("/api/energy-mix").subscribe(result => {
+      const today = result.days[0];
+      this.energyMix = {
+        date: result.startDate,
+        cleanEnergy: today.avgCleanEnergy * 100,
+        chartData: Object.keys(today)
+          .filter((key: string) => key !== "avgCleanEnergy")
+          .map((key: string)=> {
+            return {
+              name: key.substring(0, 1).toUpperCase()+ key.substring(1),
+              value: (<any>today)[key] * 100
+            };
+          })
+      };
+
+      // this.energyMix = Object.keys(result.days[0])
+      //   .filter((key: string) => key !== "avgCleanEnergy")
+      //   .map((key: string) => {
+      //   return {
+      //     name: key.substring(0, 1).toUpperCase()+ key.substring(1),
+      //     value: (<any>result.days[0])[key]
+      //   };
+      // });
+    });
+
+  }
+
+  protected readonly Math = Math;
+}
+
+interface EnergyMix {
+  date: string;
+  cleanEnergy: number;
+  chartData: {name: string; value: number}[];
+}
+
+interface EnergyMixDto {
+  startDate: string;
+  days: EnergyMixDayDto[];
+}
+
+interface EnergyMixDayDto {
+  biomass: number;
+  coal: number;
+  imports: number;
+  gas: number;
+  other: number;
+  nuclear: number;
+  hydro: number;
+  solar: number;
+  wind: number;
+  avgCleanEnergy: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { NgxChartsModule } from "@swimlane/ngx-charts";
+import { provideHttpClient } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { NgxChartsModule } from "@swimlane/ngx-charts";
     BrowserModule,
     NgxChartsModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
