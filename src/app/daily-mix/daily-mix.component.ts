61d670e3b643c0ea668a838dc282f5eee318718f
lilithhtilil <swiecaweronika6@gmail.com>
2025-11-23 22:48:42 +0100

    Modified style

--- src/app/daily-mix/daily-mix.component.ts
+++ src/app/daily-mix/daily-mix.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { EnergyMix } from "../energy-mix";
+import { Color, ScaleType } from "@swimlane/ngx-charts";
 
 @Component({
   selector: 'app-daily-mix',
@@ -13,6 +14,13 @@ export class DailyMixComponent {
   @Input() energyMix: EnergyMix | null = null;
   @Input() size: [number, number] = [200, 200];
   @Input() showLegend: boolean = false;
+
+  colorScheme: Color = {
+    name: "green",
+    selectable: false,
+    group: ScaleType.Linear,
+    domain: ['#A1D99B', '#4E4E4E', '#685A5A', '#BFBFBF', '#8C7878', '#41AB5D', '#006D2C', '#C7E9C0', '#74C476']
+  }
 
   loadingData = [
     {
